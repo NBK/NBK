b21559636e056532464f27cdba361afab21241a9
Max Hayman <maxhayman@maxhayman.co.uk>
2010-04-13 20:40:21 +0000

    *Little fix with claim effect. TODO: Use proper animation.

--- data/resources/effects/claimEffect/particle_init.lua
+++ data/resources/effects/claimEffect/particle_init.lua
@@ -28,7 +28,7 @@ function onParticleInit(particleID,particleCount)
 	--PARTICLE_SET_LIFE_PROPERTIES(1000,0.0);
 	PARTICLE_SET_SIZE_PROPERTIES(2.5,0.0,0.0);
 	--PARTICLE_SET_GRAVITY(0.0,-0.000015,0.0);
-	PARTICLE_SET_REBORN(1);
+	PARTICLE_SET_REBORN(0);
 	PARTICLE_SET_RESET_ALPHA(1);
 	-- third parameter tells interpeter to connect alpha with particles life.  if it's < 0.0f then it's connected else a valid second param should be set
 	PARTICLE_SET_ALPHA_PROPERTIES(1.0,0.0,-1.0);
